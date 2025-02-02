623f70638f5cb5438cb84563091a1448d18a1ffc
Constantijn Bok <c.bok@student.tue.nl>
2025-02-02 21:38:53 +0100

    Development

--- src/Layoutmaker.ts
+++ src/Layoutmaker.ts
@@ -12,8 +12,8 @@ export class layout {
         this.layout_num = layout_num;
     }
 
-    additem(type: string, id: Number){
-        const item_ = new item(type, id, this.curr_col, this.curr_row)
+    additem(type: string, id: Number, engine: engine){
+        const item_ = new item(type, id, this.curr_col, this.curr_row, engine)
         this.items.push(item_);
     }
 
@@ -38,13 +38,51 @@ export class item {
     row: Number;
     width: Number;
     height: Number;
+    engine: engine;
 
-    constructor(type: string, id: Number, col: Number, row: Number, width = 20, height = 20) {
+
+    constructor(type: string, id: Number, col: Number,  row: Number, engine: engine, width = 20, height = 20) {
         this.type = type;
         this.id = id;
         this.col = col;
         this.row = row;
         this.width = width;
         this.height = height;
+        this.engine = engine;
     }
+}
+
+export class engine {
+    engine_num: Number;
+    positions!: Array<Number>;
+    matricks_num!: Number;
+    phasers!: Array<phaser>;
+
+    constructor(engine_num: Number){
+        this.engine_num = engine_num
+    }
+
+
+}
+
+export class phaser {
+    
+}
+
+export class tracker {
+    curr_sequence: Number;
+    curr_macro: Number;
+
+    constructor(start_sequence: Number, start_macro: Number){
+        this.curr_sequence = start_sequence;
+        this.curr_macro = start_macro;
+    }
+
+    increment_sequence(){
+        this.curr_sequence =+ 1;
+    }
+    inrement_macro(){
+        this.curr_macro =+ 1;
+    }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { Logger, LogLevel} from "@ma3-pro-plugins/ma3-pro-plugins-lib"
 import { ImageLibraryInstaller } from "./ImageLibraryInstaller"
-import {item, layout} from "./Layoutmaker"
+import {engine, item, layout} from "./Layoutmaker"
+import { command } from "ftp"
+
+let engines = [];
 
 function main(this: void, displayHandle: Display, argument: string) {
 
@@ -10,11 +13,38 @@ function main(this: void, displayHandle: Display, argument: string) {
     switch (argument) {
         case "install": {
             ImageLibraryInstaller(log).onInstall()
+
+            for (let i = 0; i<2; i++){
+                engines.push(new engine(i+1))
+            }
+            log.trace("main(): Engines prepared and ready for initialization")
             break
         }
         case "uninstall": {
             ImageLibraryInstaller(log).onUninstall()
             break
+        }
+        case "edit": {
+            let options = {
+                title: "Setup",
+                commands: [
+                    {value: 0, name: "Cancel"},
+                    {value: 1, name: "Update"}
+                ],
+                inputs: [
+                    {name: "Group 1 Linear", value: "gr1"},
+                    {name: "Group 1 Grid", value: "gr2"},
+                    {name: "Group 2 Linear", value:"gr3"},
+                    {name: "Group 2 Grid", value:"gr4"}
+
+
+                ]
+            }
+            let input = MessageBox(options)
+            if (input.result == 1) {
+                log.trace(input.inputs["Group 1 Linear"])
+            }
+            
         }
     }
 

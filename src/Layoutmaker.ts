773e2302b8723386fcd6f2f4915840dcc5fcc364
Constantijn Bok <c.bok@student.tue.nl>
2025-02-04 00:50:10 +0100

    development

--- src/Layoutmaker.ts
+++ src/Layoutmaker.ts
@@ -53,36 +53,142 @@ export class item {
 }
 
 export class engine {
-    engine_num: Number;
-    positions!: Array<Number>;
-    matricks_num!: Number;
+    engine_num: number;
+    positions!: Array<number>;
+    palette_pos!: Array<number>;
+    matricks_flyout_num!: number;
+    matricks_movement_num!: number;
+    matricks_pos!: number;
     phasers!: Array<phaser>;
+    group_linear!: number;
+    group_grid!: number;
 
-    constructor(engine_num: Number){
+    constructor(engine_num: number){
         this.engine_num = engine_num
     }
 
+    tostring() {
+        let dict = [];
+        dict.push(this.engine_num);
+        dict.push(this.positions.length)
+        this.positions.forEach(e =>{
+            dict.push(e)
+        })
+
+        return dict.join(",") // engine_num , length pos array , pos array, 
+    }
+
+    create_engine(track: tracker, preset: presets){
+        for (let i = 0; i<2; i++){ // create custom palettes for flyout 
+            Cmd("Store preset 2."+track.curr_pos+" /nc");
+            Cmd("Store label 2."+track.curr_pos+" POS_ENGINE_"+this.engine_num+"_PALETTE_"+(i+1)+" /nc");
+            this.palette_pos[i] = track.curr_pos;
+            track.increment_pos;
+        }
+    }
 
 }
 
 export class phaser {
-    
+    cross: String;
+    step1_width: Number;
+    step2_width: Number;
+    step1_size: Number;
+    step2_size: Number;
+    step1_palette!: Number;
+    step2_palette!: Number;
+
+    constructor(cross: String, step1_width: Number, step2_width: Number, step1_size: Number, step2_size:Number) {
+        this.cross = cross;
+        this.step1_size = step1_size;
+        this.step2_size = step2_size;
+        this.step1_width = step1_width;
+        this.step2_width = step2_width;
+    }
+
+    create_phaser(){
+
+    }
 }
 
 export class tracker {
-    curr_sequence: Number;
-    curr_macro: Number;
+    curr_sequence: number;
+    curr_macro: number;
+    curr_pos: number;
 
-    constructor(start_sequence: Number, start_macro: Number){
+    constructor(start_sequence: number, start_macro: number, curr_pos: number){
         this.curr_sequence = start_sequence;
         this.curr_macro = start_macro;
-    }
+        this.curr_pos = curr_pos;
+    };
 
     increment_sequence(){
-        this.curr_sequence =+ 1;
-    }
+        this.curr_sequence = this.curr_sequence + 1;
+    };
     inrement_macro(){
-        this.curr_macro =+ 1;
+        this.curr_macro = this.curr_macro + 1;
+    };
+    increment_pos(){
+        this.curr_pos = this.curr_pos + 1;
+    };
+    tostring(){
+        return [this.curr_sequence, this.curr_macro, this.curr_pos].join(",") 
     }
+}
+
+export function trackerfromstring(string: string | undefined){
+    if (!string){
+        error("main(): Tracker object was not found, please reinstall plugin")
+        StopProgress
+    }
+    let s = string.split(",")
+    let curr_sequence = parseFloat(s[0]);
+    let curr_macro = parseFloat(s[1]);
+    let curr_pos = parseFloat(s[2]);
+    return new tracker(curr_sequence, curr_macro, curr_pos);
+}
+
+export class presets { // Presets used for all engines 
+    positions: Number[] = [-1,-1]; //Begin and end number 
+
+    set_position_start(n: Number){
+        this.positions[0] = n;
+    }
+    set_position_end(n: Number){
+        this.positions[1] = n;
+    }
+    tostring(){
+        return this.positions.join(",")
+    }
+
+}
+
+const positions = {
+    0: "LOWFRONT",
+    1: "LOWPOINT",
+    2: "LOWSPREAD",
+    3: "MIDFRONT",
+    4: "MIDPOINT",
+    5: "MIDSPREAD",
+    6: "UPFRONT",
+    7: "UPPOINT",
+    8: "UPSPREAD"
+};
+
+export function clearprogrammer() {
+    Cmd("clear; clear; clear")
+}
+
+export function create_positions(track: tracker, presets: presets){
+    presets.set_position_start(track.curr_pos)
+    Object.values(positions).forEach(p => {
+        Cmd("Store preset 2."+track.curr_pos+" /nc");
+        Cmd("Label preset 2."+track.curr_pos+" "+ p +" /nc");
+        track.increment_pos();
+    }); 
+    presets.set_position_end(track.curr_pos)
+}
+
+export function parse_engines(){ // will find engines in variable list and return array with all engines
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,77 @@
 import { Logger, LogLevel} from "@ma3-pro-plugins/ma3-pro-plugins-lib"
 import { ImageLibraryInstaller } from "./ImageLibraryInstaller"
-import {engine, item, layout} from "./Layoutmaker"
+import {clearprogrammer, create_positions, engine, trackerfromstring, item, layout, presets, tracker} from "./Layoutmaker"
 import { command } from "ftp"
 
-let engines = [];
+let engines: Array<engine> = [];
 
 function main(this: void, displayHandle: Display, argument: string) {
 
     const log = Logger({ prefix: ["PluginTemplate"], logLevel: LogLevel.TRACE })
     log.trace("main(): Plugin started")
 
-    switch (argument) {
-        case "install": {
-            ImageLibraryInstaller(log).onInstall()
-
-            for (let i = 0; i<2; i++){
-                engines.push(new engine(i+1))
+    if (GetVar(UserVars(), "POS_ENGINE_INSTALLED")){
+        switch (argument) {
+            case "uninstall": {
+                ImageLibraryInstaller(log).onUninstall()
+                break
             }
-            log.trace("main(): Engines prepared and ready for initialization")
-            break
-        }
-        case "uninstall": {
-            ImageLibraryInstaller(log).onUninstall()
-            break
-        }
-        case "edit": {
-            let options = {
-                title: "Setup",
-                commands: [
-                    {value: 0, name: "Cancel"},
-                    {value: 1, name: "Update"}
-                ],
-                inputs: [
-                    {name: "Group 1 Linear", value: "gr1"},
-                    {name: "Group 1 Grid", value: "gr2"},
-                    {name: "Group 2 Linear", value:"gr3"},
-                    {name: "Group 2 Grid", value:"gr4"}
+            case "startshow": { // objects should be loaded here
 
+            }
+            case null: {
+                    
+                let options = {
+                    title: "Setup",
+                    commands: [
+                        {value: 0, name: "Cancel"},
+                        {value: 1, name: "Update"},
+                    ],
+                    inputs: [
+                        {name: "Group 1 Linear", value: "gr1"},
+                        {name: "Group 1 Grid", value: "gr2"},
+                        {name: "Group 2 Linear", value:"gr3"},
+                        {name: "Group 2 Grid", value:"gr4"}
+                    ]
+                }
+                let input = MessageBox(options)
+                
+                log.trace("main(): Creating empty engines")
+                let engines: Array<engine> = [new engine(1), new engine(2)];
 
-                ]
+                log.trace("main(): Assigning groups to engines")
+                if (input.result == 1) {
+                    Object.keys(input.inputs).forEach((e) => {
+                        if ((input.inputs[e] && e)){
+                            let index = parseFloat(e.split(" ")[1]);
+                            engines[index-1].engine_num = parseFloat(input.inputs[e])
+                        }
+                    });
+                    let track = trackerfromstring(GetVar(UserVars(), "POS_ENGINE_TRACKER"))
+                    
+                    engines.forEach(e => {
+                        e.create_engine(track, preset)
+                    })
+                }
+            break  
             }
-            let input = MessageBox(options)
-            if (input.result == 1) {
-                log.trace(input.inputs["Group 1 Linear"])
-            }
-            
         }
+    }else{
+        log.trace("main(): Starting installation");
+        log.trace("main(): Clearing programmer");
+        clearprogrammer();
+        log.trace("main(): Install images");
+        ImageLibraryInstaller(log).onInstall();
+        log.trace("main(): prepeare objects");
+        let track = new tracker(100, 100, 100);
+        let preset = new presets();
+        log.trace("main(): Create engines");
+        create_positions(track, preset);
+        SetVar(UserVars(), "POS_ENGINE_TRACKER", track.tostring());
+        SetVar(UserVars(),"POS_ENGINE_PRESET", preset.tostring());
+        SetVar(UserVars(), "POS_ENGINE_INSTALLED", true)
+        log.trace("main(): Engines prepared and ready for initialization")
     }
-
 }
 
 export default [main]
